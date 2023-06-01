55a4527a3f48aa3688a2ee14452181dfcffecb0b
gustavo acciari <127262251+Guacciari@users.noreply.github.com>
2023-06-01 14:02:24 -0300

    Update ListaTema.tsx

--- src/components/temas/listatema/ListaTema.tsx
+++ src/components/temas/listatema/ListaTema.tsx
@@ -1,51 +1,72 @@
-import React, {useState, useEffect} from 'react'
-import { Link } from 'react-router-dom'
-import {Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
-import {Box} from '@mui/material';
-import Tema from '../../../models/Tema';
-import './ListaTema.css';
-import useLocalStorage from 'react-use-localstorage';
-import {useNavigate} from 'react-router-dom';
-import { busca } from '../../../services/Service';
+import { Box, Card, CardContent, Typography, CardActions, Button } from '@mui/material';
+import React, { useState,useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { Tema } from '../../../model/Tema';
+import { busca } from '../../../service/service';
+import { useSelector, useDispatch } from 'react-redux';
+import { addToken } from '../../../store/tokens/action';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import './ListaTema.css'
 
-function ListaTema() {
+function ListaTemas() {
   const [temas, setTemas] = useState<Tema[]>([])
-  const [token, setToken] = useLocalStorage('token');
-  let navigate = useNavigate();
+  const navigate = useNavigate();
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state) => state.tokens
+  );
 
-  useEffect(()=>{
-    if(token == ''){
-      alert("Você precisa estar logado")
-      navigate("/login")
-    }
-  }, [token])
-
-
-  async function getTema(){
-    await busca("/tema", setTemas, {
-      headers: {
-        'Authorization': token
+  const dispatch = useDispatch()
+  
+  async function getTemas() {
+    // alterado a função pra dentro de um try catch, para poder verificar a validade do token do usuário
+    try {
+      // a parte do TRY, fica igual ao que ja tinha antes
+      await busca('/temas', setTemas, {
+        headers: {
+          Authorization: token
+        }
+      })
+    } catch (error: any) {
+      // a parte do catch, vai receber qlquer mensagem de erro que chegue, e caso a mensagem tenha um 403 no seu texto
+      // significa que o token já expirou. Iremos alertar o usuário sobre isso, apagar o token do navegador, e levá-lo para a tela de login
+      if(error.toString().includes('403')) {
+        console.log(error);
+        alert('O seu token expirou, logue novamente')
+        dispatch(addToken(''))
+        navigate('/login')
       }
-    })
+    }
   }
+  
+  useEffect(() => {
+    getTemas()
+  }, [])
 
-
-  useEffect(()=>{
-    getTema()
-  }, [temas.length])
-
+  useEffect(() => {
+    if(token === ''){ 
+      alert('Ta tirando né??? sem token não rola')
+      navigate('/login')
+    }
+  }, [])
+  
   return (
     <>
-    {
-      temas.map(tema =>(
-      <Box m={2} >
+    {/* criando um if ternário para exibir um loader de carregamento enquanto os temas não chegam do backend */}
+    {/* na primeira linha, temos a condição do if */}
+      {temas.length === 0 
+      // com o sinal de interrogação, fazemos a saida padrão do if, para caso a condição seja verdadeira
+        ? <div className="alinhamento"><span className="loader"></span></div> 
+        // o dois pontos (:) representa o ELSE de um if padrão, e colocamos a saida para caso a condição seja falsa. Nesse caso, exibir nada
+        : <></>}
+      {temas.map((tema) => (
+        <Box m={2} >
         <Card variant="outlined">
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
-              Tema
+              Tema numero {tema.id}
             </Typography>
             <Typography variant="h5" component="h2">
-             {tema.descricao}
+              Descrição do tema: {tema.descricao}
             </Typography>
           </CardContent>
           <CardActions>
@@ -58,7 +79,7 @@ function ListaTema() {
                   </Button>
                 </Box>
               </Link>
-              <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
+              <Link to={`/apagarTema/${tema.id}`} className="text-decorator-none">
                 <Box mx={1}>
                   <Button variant="contained" size='small' color="secondary">
                     deletar
@@ -69,11 +90,9 @@ function ListaTema() {
           </CardActions>
         </Card>
       </Box>
-      ))
-      }
+      ))}
     </>
   );
 }
 
-
-export default ListaTema;
+export default ListaTemas
